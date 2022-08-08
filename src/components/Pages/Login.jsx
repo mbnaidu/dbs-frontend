ca1f748cc47b3016ecf0f82283cd52f50cddc416
mb-charlie <92309564+mb-charlie@users.noreply.github.com>
2022-08-08 21:27:32 +0530

    designed login page

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,77 +1,46 @@
-import { Button, TextField } from '@mui/material'
-import React, { useState } from 'react'
-import {
-    useNavigate,
-} from "react-router-dom";
+import React from 'react';
 import "../../Styles/Login.css"
+import dbsLogo from '../../Assets/dbs-bank-logo.png';
 
 export default function Login() {
-    let navigate = useNavigate();
-    const [UserName, setUserName] = useState("");
-    const [Password, setPassword] = useState("");
     const handleLogin = () => {
-        localStorage.setItem("session", UserName);
+        localStorage.setItem("session", document.getElementById("username").value);
         window.location.reload();
     };
     const FormHeader = props => (
-        <h2 id="headerTitle">{props.title}</h2>
+        <h2 id="headerTitle">
+            <img src={dbsLogo} alt="logo" width="300" height="300" />
+        </h2>
     );
 
 
     const Form = props => (
         <div>
-            <FormInput description="Username" placeholder="Enter your username" type="text" />
-            <FormInput description="Password" placeholder="Enter your password" type="password" />
+            <FormInput description="Username" placeholder="Enter your username" type="text" id="username" />
+            <FormInput description="Password" placeholder="Enter your password" type="password" id="password" />
             <FormButton title="Log in" />
         </div>
     );
 
     const FormButton = props => (
-        <div id="button" class="row">
-            <button>{props.title}</button>
+        <div id="button" className="row">
+            <button onClick={() => handleLogin()}>{props.title}</button>
         </div>
     );
 
     const FormInput = props => (
-        <div class="row">
+        <div className="row">
             <label>{props.description}</label>
-            <input type={props.type} placeholder={props.placeholder} />
+            <input type={props.type} placeholder={props.placeholder} id={props.id} />
         </div>
-    );
-
-    const OtherMethods = props => (
-        <div id="alternativeLogin">
-            <label>Or sign in with:</label>
-            <div id="iconGroup">
-                <Facebook />
-                <Twitter />
-                <Google />
-            </div>
-        </div>
-    );
-
-    const Facebook = props => (
-        <a href="#" id="facebookIcon"></a>
-    );
-
-    const Twitter = props => (
-        <a href="#" id="twitterIcon"></a>
-    );
-
-    const Google = props => (
-        <a href="#" id="googleIcon"></a>
     );
 
     return (
-        <div>
+        <div style={{ paddingTop: window.innerHeight - 800 }}>
             <div id="loginform">
                 <FormHeader title="Login" />
                 <Form />
-                <OtherMethods />
             </div>
-            {/* <TextField id="standard-basic" label="Username" variant="standard" value={UserName} onChange={(e) => setUserName(e.target.value)} required /><br />
-            <TextField id="standard-basic" label="Password" variant="standard" value={Password} onChange={(e) => setPassword(e.target.value)} required /><br />
-            <Button onClick={() => handleLogin()}>Login</Button> */}
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from "./App";
 import Login from './components/Pages/Login';
+import Header from './Contents/Header';
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 const UserExists = localStorage.getItem("session")
 root.render(
     <BrowserRouter>
+        {UserExists && <Header />}
         <Routes>
             <Route path="/" element={UserExists ? <App /> : <Login />} />
         </Routes>
