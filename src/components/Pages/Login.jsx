1d435a8d9823b9d542aaefbfb63eb8168d9f2da0
mb-charlie <92309564+mb-charlie@users.noreply.github.com>
2022-08-08 17:14:38 +0530

    created new login design

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import {
     useNavigate,
 } from "react-router-dom";
+import "../../Styles/Login.css"
 
 export default function Login() {
     let navigate = useNavigate();
@@ -11,12 +12,66 @@ export default function Login() {
     const handleLogin = () => {
         localStorage.setItem("session", UserName);
         window.location.reload();
-    }
+    };
+    const FormHeader = props => (
+        <h2 id="headerTitle">{props.title}</h2>
+    );
+
+
+    const Form = props => (
+        <div>
+            <FormInput description="Username" placeholder="Enter your username" type="text" />
+            <FormInput description="Password" placeholder="Enter your password" type="password" />
+            <FormButton title="Log in" />
+        </div>
+    );
+
+    const FormButton = props => (
+        <div id="button" class="row">
+            <button>{props.title}</button>
+        </div>
+    );
+
+    const FormInput = props => (
+        <div class="row">
+            <label>{props.description}</label>
+            <input type={props.type} placeholder={props.placeholder} />
+        </div>
+    );
+
+    const OtherMethods = props => (
+        <div id="alternativeLogin">
+            <label>Or sign in with:</label>
+            <div id="iconGroup">
+                <Facebook />
+                <Twitter />
+                <Google />
+            </div>
+        </div>
+    );
+
+    const Facebook = props => (
+        <a href="#" id="facebookIcon"></a>
+    );
+
+    const Twitter = props => (
+        <a href="#" id="twitterIcon"></a>
+    );
+
+    const Google = props => (
+        <a href="#" id="googleIcon"></a>
+    );
+
     return (
         <div>
-            <TextField id="standard-basic" label="Username" variant="standard" value={UserName} onChange={(e) => setUserName(e.target.value)} required /><br />
+            <div id="loginform">
+                <FormHeader title="Login" />
+                <Form />
+                <OtherMethods />
+            </div>
+            {/* <TextField id="standard-basic" label="Username" variant="standard" value={UserName} onChange={(e) => setUserName(e.target.value)} required /><br />
             <TextField id="standard-basic" label="Password" variant="standard" value={Password} onChange={(e) => setPassword(e.target.value)} required /><br />
-            <Button onClick={() => handleLogin()}>Login</Button>
+            <Button onClick={() => handleLogin()}>Login</Button> */}
         </div>
     )
 }
